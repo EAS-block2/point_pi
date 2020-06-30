f0328a7478d1f2bc492de333e67e852980c9f881
Jacob Ellington <jake@workstation.ellingtech.com>
2020-06-30 13:51:30 -0400

    Basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
 //Digital Signage and Strobe systems
+use std::time::Duration;
+use std::net::{TcpListener};
+use std::io::{Read, Write};
+use std::str;
 fn main() {
-    println!("Hello, world!");
+
+let listener = TcpListener::bind("0.0.0.0:5400").unwrap();
+for stream in listener.incoming() {
+    match stream {
+        Ok(mut streamm) => {
+            let mut data = [0 as u8; 50];
+            match streamm.read(&mut data){
+                Ok(size) => {
+                   match str::from_utf8(&data[0..size]){
+                       Ok(string_out) => {
+                           println!("Got data: {}", string_out);
+                           streamm.write(b"ok").unwrap();
+                       }
+                       Err(_) => {println!("fault");}
+                   }
+                }
+                Err(_) => {println!("Fault when reading data!");}
+            }
+        }
+        Err(e) => {println!("Connection failed with code {}", e);}
+    }
+}
 }
